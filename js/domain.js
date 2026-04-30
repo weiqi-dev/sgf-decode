86a671e564d1895c24e2b2d3b62e3652b8e92ddb
root <root@localhost.localdomain>
2026-04-30 12:02:50 +0800

    chore: add domain config

---
+++ js/domain.js
@@ -0,0 +1,11 @@
+// 自动生成的域名配置
+// 生成时间: 2026-04-30T11:59:09+08:00
+// 更新域名: https://worldcat-richard-walks-fairfield.trycloudflare.com
+
+const API_DOMAIN = "https://worldcat-richard-walks-fairfield.trycloudflare.com";
+const API_BASE = "https://https://worldcat-richard-walks-fairfield.trycloudflare.com:9801";
+
+// 兼容性导出
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { API_DOMAIN, API_BASE };
+}
